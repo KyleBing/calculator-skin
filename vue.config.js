6179673b02a6ddea71cf865534014fe30218b9c8
邴新科 <kylebing@163.com>
2022-03-15 16:31:05 +0800

    output dir

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: './', // css js 文件基于输出目录，而不是网站主目录
     productionSourceMap: false, // 不产出 .map 文件
-    outputDir: './calculator',
+    outputDir: '../calculator',
 }
